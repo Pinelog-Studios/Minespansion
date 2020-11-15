f4b3a505ef28c115784f5b0ef71d69f0a03896d1
Mr. Pineapple <mr.sad.pineapple@gmail.com>
2020-11-15 18:14:22 +0000

    Added Silver Items
    Added all Silver Items

---
+++ src/main/java/co/uk/pinelogstudios/minespansion/common/materials/ArmorMaterialList.java
@@ -0,0 +1,79 @@
+package co.uk.pinelogstudios.minespansion.common.materials;
+
+import co.uk.pinelogstudios.minespansion.core.Minespansion;
+import co.uk.pinelogstudios.minespansion.core.registry.ItemList;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.Supplier;
+
+/*
+ * Author: Mr. Pineapple
+ */
+public enum ArmorMaterialList implements IArmorMaterial {
+    SILVER(Minespansion.ID + ":silver", 15, new int[]{2, 5, 6, 2}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> {
+        return Ingredient.fromItems(ItemList.SILVER_INGOT.get());
+    });
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    private ArmorMaterialList(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountArrayIn, int enchantabilityIn, SoundEvent soundEventIn, float toughnessIn, float knockbackResistanceIn, Supplier<Ingredient> repairMaterialIn) {
+        this.name = nameIn;
+        this.maxDamageFactor = maxDamageFactorIn;
+        this.damageReductionAmountArray = damageReductionAmountArrayIn;
+        this.enchantability = enchantabilityIn;
+        this.soundEvent = soundEventIn;
+        this.toughness = toughnessIn;
+        this.knockbackResistance = knockbackResistanceIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public String getName() {
+        return this.name;
+    }
+
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/co/uk/pinelogstudios/minespansion/core/registry/ItemList.java
+++ src/main/java/co/uk/pinelogstudios/minespansion/core/registry/ItemList.java
@@ -1,7 +1,10 @@
 package co.uk.pinelogstudios.minespansion.core.registry;
 
+import co.uk.pinelogstudios.minespansion.common.materials.ArmorMaterialList;
 import co.uk.pinelogstudios.minespansion.core.Minespansion;
-import net.minecraft.item.Item;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.*;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,5 +19,15 @@ public class ItemList {
     public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(item));
     public static final RegistryObject<Item> SILVER_NUGGET = ITEMS.register("silver_nugget", () -> new Item(item));
 
+    public static final RegistryObject<Item> SILVER_HELMET = ITEMS.register("silver_helmet", () -> new ArmorItem(ArmorMaterialList.SILVER, EquipmentSlotType.HEAD, item));
+    public static final RegistryObject<Item> SILVER_CHESTPLATE = ITEMS.register("silver_chestplate", () -> new ArmorItem(ArmorMaterialList.SILVER, EquipmentSlotType.CHEST, item));
+    public static final RegistryObject<Item> SILVER_LEGGINGS = ITEMS.register("silver_leggings", () -> new ArmorItem(ArmorMaterialList.SILVER, EquipmentSlotType.LEGS, item));
+    public static final RegistryObject<Item> SILVER_BOOTS = ITEMS.register("silver_boots", () -> new ArmorItem(ArmorMaterialList.SILVER, EquipmentSlotType.FEET, item));
+
+    public static final RegistryObject<Item> SILVER_SWORD = ITEMS.register("silver_sword", () -> new SwordItem(ItemTier.IRON, 3, -2.4F, item));
+    public static final RegistryObject<Item> SILVER_PICKAXE = ITEMS.register("silver_pickaxe", () -> new PickaxeItem(ItemTier.IRON, 1, -2.8F, new Item.Properties().addToolType(ToolType.PICKAXE, 2).group(Minespansion.MINESPANSION_TAB)));
+    public static final RegistryObject<Item> SILVER_AXE = ITEMS.register("silver_axe", () -> new AxeItem(ItemTier.IRON, 6, -3.1F, new Item.Properties().addToolType(ToolType.AXE, 2).group(Minespansion.MINESPANSION_TAB)));
+    public static final RegistryObject<Item> SILVER_SHOVEL = ITEMS.register("silver_shovel", () -> new ShovelItem(ItemTier.IRON, 1.5F, -3, new Item.Properties().addToolType(ToolType.SHOVEL, 2).group(Minespansion.MINESPANSION_TAB)));
+    public static final RegistryObject<Item> SILVER_HOE = ITEMS.register("silver_hoe", () -> new HoeItem(ItemTier.IRON, -2, -1.0F, new Item.Properties().addToolType(ToolType.HOE, 2).group(Minespansion.MINESPANSION_TAB)){});
 
 }
